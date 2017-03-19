d76ee83ab486925d8edcd93573bfbafce6f56cbd
unknown <chenchalaphp@gmail.com>
2017-03-19 17:19:17 +0530

    validations

--- app/scripts/controllers/studentCtrl.js
+++ app/scripts/controllers/studentCtrl.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('schoolApp')
-    .controller('StudentCtrl', function($scope,$timeout, ServerCall,fileUpload,CommonService) {
+    .controller('StudentCtrl', function($scope, $timeout, ServerCall, fileUpload, CommonService) {
         $scope.gender = 'male';
-        $scope.classList=CommonService.getClassList();
+        $scope.classList = CommonService.getClassList();
         var stdListSuccess = function(res) {
-                $scope.stdList = res;
-            }
+            $scope.stdList = res;
+        }
         var stdListError = function(res) {
             debugger;
         }
@@ -15,16 +15,17 @@ angular.module('schoolApp')
             ServerCall.getData('student/' + selClassId, 'GET', '', stdListSuccess, stdListError);
 
         }
-        var stdRegSuccess=function(res){
+        var stdRegSuccess = function(res) {
 
         }
-        var stdRegError=function(res){
+        var stdRegError = function(res) {
 
         }
+
         $scope.fnRegisterStudent = function() {
                   
 
-             if($('#studentRegistration').valid()){
+            if ($('#studentRegistration').valid()) {
                 var fd = new FormData();
                 fd.append('Image', $scope.stdImg);
                 fd.append('Email', $scope.parentEmail);
@@ -35,37 +36,38 @@ angular.module('schoolApp')
                 fd.append('RollNumber', $scope.rollNo);
                 fd.append('Discount', 0);
                 fd.append('ClassID', $scope.selClass);
-                fileUpload.uploadData('registration/'+$scope.selClass+'/student',fd,stdRegSuccess,stdRegError)
-             }
+                fileUpload.uploadData('registration/' + $scope.selClass + '/student', fd, stdRegSuccess, stdRegError)
+            }
 
         }
-         $timeout(function(){
-                    $('#studentRegistration').validate({
-                    rules: {
-                        'firstName': 'required',
-                        'lastName': 'required',
-                        'parentEmail':{
-                            'required':true,
-                            'parentEmail':true
-                            } ,
-                        'rollNumber':'required',
-                         'class':'required',
-                         'dob':'required',
-                         'gender':'required',
-                         'stdImg' :'required'
-                     },
-                    messages: {
-                        'firstName': 'Please Enter First Name',
-                        'lastName': 'Please Enter lastName',
-                        'parentEmail': 'Please Enter Correct Email',
-                        'rollNumber': 'Please Enter RollNumber',
-                        'class': 'Please Enter class',
-                        'dob':'Please Select DateOfBirth',
-                        'gender':'Please Select Gender',
-                        'stdImg' :'Please Upload Image'
-                    }
-                }); 
-                },1000);
+        $timeout(function() {
+            $('#studentRegistration').validate({
+               
+                rules: {
+                    'firstName': 'required',
+                    'lastName': 'required',
+                    'parentEmail': {
+                        'required': true,
+                        'parentEmail': true
+                    },
+                    'rollNumber': 'required',
+                    'class': 'required',
+                    'dob': 'required',
+                    'gender': 'required',
+                    'stdImg': 'required'
+                },
+                messages: {
+                    'firstName': 'Please Enter First Name',
+                    'lastName': 'Please Enter lastName',
+                    'parentEmail': 'Please Enter Correct Email',
+                    'rollNumber': 'Please Enter RollNumber',
+                    'class': 'Please Enter class',
+                    'dob': 'Please Select DateOfBirth',
+                    'gender': 'Please Select Gender',
+                    'stdImg': 'Please Upload Image'
+                }
+            });
+        }, 1000);
 
 
 
